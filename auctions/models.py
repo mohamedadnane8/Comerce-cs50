9855a09e8057971105e535403b8aa6cf5a319cec
mohamedadnane8 <mohamed.adnane8@gmail.com>
2020-08-24 03:17:49 +0100

    modified the models and created html page for creating listings

--- auctions/models.py
+++ auctions/models.py
@@ -2,24 +2,37 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
 class User(AbstractUser):
     # it inherits from the the AbstractUser (so it already have the username password and email!)
     pass
+
+
 class Bid(models.Model):
     price = models.FloatField()
-    user = models.ForeignKey(User,on_delete= models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     date = models.DateTimeField(auto_now_add=True)
-    
+    # TO DO: Have to fix the default
+    # I have to fix this default
+    auction_listing = models.ForeignKey(
+        "AuctionListing", on_delete=models.CASCADE, default="Bid(price=0)"
+    )
+
+
 class AuctionListing(models.Model):
     image = models.ImageField()
     details = models.TextField(max_length=400)
-    title  = models.CharField(max_length=60)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
     date = models.DateTimeField(auto_now_add=True)
-    bid = models.ForeignKey(Bid, on_delete=models.CASCADE)
+
 
 class Comment(models.Model):
-    comment  = models.CharField(max_length=200)
-    user = models.ForeignKey(User,on_delete= models.DO_NOTHING)
+    comment = models.CharField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    auction_listing = models.ForeignKey("AuctionListing", on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
-    
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    auction_listing = models.ManyToManyField(AuctionListing)
